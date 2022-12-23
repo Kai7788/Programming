fa4e0a82adccc1be51b1af5e8a6485025bb90f8a
Kai Maier <Kaimaier2002@yahoo.com>
2022-12-23 16:35:19 +0100

    Added basic information of how Attributes work!

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+import sys
+
+#Declare the function called 'main()'
+def main(argv):
+    list_length = len(argv)
+    print("The list has " +str(list_length) + " attributes!")
+    counter = 0
+    for x in argv:
+        print("The " + str(counter) + " value of the inputed arguments is: " + str(x))
+        counter += 1
+
+#If this Script is executed the attribute __name__ will change to "__main__ and thus call the main funcion"
+if __name__ == "__main__":
+    #Call the main function
+    main(sys.argv)
